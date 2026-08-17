bd66e130a93f0d505f18651b1112dcd9a2f8fee9
Nemat Sachdeva <nematsachdevacollege0009@gmail.com>
2026-08-17 23:03:54 +0530

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        int flag=-1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (nums[mid] == target){
+                flag= mid;
+                break;
+        }
+
+            // If x greater, ignore left half
+            if (nums[mid] < target){
+                low = mid + 1;
+            }
+
+            // If x is smaller, ignore right half
+            if(nums[mid]>target){
+                high = mid - 1;
+            }
+        }
+        return flag;
+    }
+}
